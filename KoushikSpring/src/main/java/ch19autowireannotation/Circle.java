acc4fc2253d129bae991aee24bda0a5856ecc259
folaulau <fkaveinga@columbususa.com>
2015-10-29 16:05:04 -0700

    baseline-1.3
    add videos.

--- KoushikSpring/src/main/java/ch19autowireannotation/Circle.java
+++ KoushikSpring/src/main/java/ch19autowireannotation/Circle.java
@@ -6,20 +6,11 @@ import org.springframework.beans.factory.annotation.Qualifier;
 public class Circle implements Shape{
 	
 	@Autowired
-	@Qualifier(value="circle-related")
+	@Qualifier(value="circle-related")// optional but its to mark a bean.
 	private Point a;
 	
 	public void draw() {
 		System.out.println("drawing a circle");
 		System.out.println("Point: "+a.toString());
 	}
-
-	public Point getA() {
-		return a;
-	}
-	
-	public void setA(Point a) {
-		this.a = a;
-	}
-
 }
--- KoushikSpring/src/main/java/ch19autowireannotation/Main.java
+++ KoushikSpring/src/main/java/ch19autowireannotation/Main.java
@@ -6,6 +6,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
 	
 	public static void main(String[] args) {
+		System.out.println("When autowiring, Spring will look for a bean defition, if more than one, then Spring will"
+				+ " look for id as name of the bean. OR user Qualified(name='something') to indicate that this is "
+				+ "the bean to use.");
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("ch19autowireannotatin.xml");
 		Shape sh =  (Shape) ctx.getBean("circle");
 		sh.draw();
